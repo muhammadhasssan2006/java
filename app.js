4a58c135b812b7e606206ae8afaf7508b017714b
hassan <hassanweb365@gmail.com>
2023-10-01 14:10:33 +0500

    --

---
+++ app.js
@@ -0,0 +1,28 @@
+alert("Hello world")
+var name ="Hassan";
+console.log(name)
+var num1="2";
+var num2="3";
+var num3="4";
+var num4="5";
+console.log(num1+num2--)
+console.log(num2)
+console.log(num4*num2)
+console.log(num3/num4)
+console.log(num3-num1)
+var name1="Hi";
+var name2=" there iam ";
+var name3="Hassan. ";
+console.log(name1+name2+name3)
+var lame="I want to";
+var lame1=" say that this is";
+var lame2="mine first js assignment.";
+var lame3="I am feeling well.";
+console.log(lame+lame1)
+console.log(lame2)
+console.log(lame3)
+var hame="Inshallah, ";
+var has1="i will complete whole js ";
+var has2="very soon. ";
+console.log(hame)
+console.log(has1+has2)
