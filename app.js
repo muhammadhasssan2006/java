8bf0cdf1cbe078e9c282567bb73045c8e2a857be
hassan <hassanweb365@gmail.com>
2023-10-14 17:43:03 +0500

    --

--- app.js
+++ app.js
@@ -1,28 +1,153 @@
-alert("Hello world")
-var name ="Hassan";
-console.log(name)
-var num1="2";
-var num2="3";
-var num3="4";
-var num4="5";
-console.log(num1+num2--)
-console.log(num2)
-console.log(num4*num2)
-console.log(num3/num4)
-console.log(num3-num1)
-var name1="Hi";
-var name2=" there iam ";
-var name3="Hassan. ";
-console.log(name1+name2+name3)
-var lame="I want to";
-var lame1=" say that this is";
-var lame2="mine first js assignment.";
-var lame3="I am feeling well.";
-console.log(lame+lame1)
-console.log(lame2)
-console.log(lame3)
-var hame="Inshallah, ";
-var has1="i will complete whole js ";
-var has2="very soon. ";
-console.log(hame)
-console.log(has1+has2)
+// alert("Hello world")
+// var name ="Hassan";
+// console.log(name)
+// var num1="2";
+// var num2="3";
+// var num3="4";
+// var num4="5";
+// console.log(num1+num2--)
+// console.log(num2)
+// console.log(num4*num2)
+// console.log(num3/num4)
+// console.log(num3-num1)
+// var name1="Hi";
+// var name2=" there iam ";
+// var name3="Hassan. ";
+// console.log(name1+name2+name3)
+// var lame="I want to";
+// var lame1=" say that this is";
+// var lame2="mine first js assignment.";
+// var lame3="I am feeling well.";
+// console.log(lame+lame1)
+// console.log(lame2)
+// console.log(lame3)
+// var hame="Inshallah, ";
+// var has1="i will complete whole js ";
+// var has2="very soon. ";
+// console.log(hame)
+// console.log(has1+has2)
+
+
+// var name = "hassan"
+// var lastName = "  suleman"
+
+// console.log("Thank you",name ,lastName)\
+
+
+// var num = "2"
+// var num2 = "2"
+
+// console.log(+"6" +  +"2");
+
+
+// var spec = prompt(name + lastName)
+
+// console.log(spec)
+
+
+// var name = "hassan "
+// var lastName = "suleman"
+
+// var student1 = "with card";
+// var student2 = "no card";
+
+// var num1 = 2
+// var num2 = 4
+
+// var sum = 6
+
+// if (student1 === "with card" ) {
+//     alert(name+"Allowed")
+// }
+// if(student2 === "no card"){
+
+//     alert(name+"Not Allowed")
+
+// }
+// if (num1 + num2 === sum) {
+//     alert("Correct")
+// }
+
+
+
+
+
+//  var a = 2
+//  var b = 3
+//  var x = 3
+//  var y = 3
+
+
+//  if (a + b !== x - y) {
+//  alert("pass")
+//  }
+
+
+// var a = "9"
+// var b = 9
+
+// if (a == b) {
+//     alert("True")
+// }
+
+
+//  var x = "3";
+//  var y = "5";
+//  var z = "15";
+//  if( x + y !== 0){
+//     alert("right")
+//  }
+
+
+// let age ="20";
+// if(age>18){
+//     console.log("you can play game")
+// }
+// else if(age==2){
+//     console.log("age is 2")
+// }
+// else if (age==4){
+//     console.log("age is 4")
+// }
+// else{
+//     console.log("you can not play game")
+// }
+
+
+// Logical operator 
+
+// && AND operator
+// || OR operator
+
+
+
+var age = "12"
+var salary = "20"
+var salary2 = "20"
+var salary3 = "25"
+
+
+// if (age && salary == "zeera") {
+//     console.log("you are Perfect")
+// }
+// else { console.log("you are not retaired") }
+
+// if (salary || salary2 == salary) {
+//     console.log("you are Perfect")
+// }
+// else { console.log("you are not retaired") }
+
+
+// if ( salary == salary2){
+//     if(age > 10){
+//         console.log("we are Here")
+//     }else{
+//         console.log("we are in nested else")
+//     }
+// }
+// else {
+//     console.log("we are in else")
+// }
+
+
+
